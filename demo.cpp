da3936a93eb5a910ebf73e97793af06136c68cf0
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-05-06 11:53:21 +0530

    TANJIRO1709

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+/*
+void printName()
+{
+    cout << "Sujal" << endl;
+}
+int sum(int num1, int num2)
+{
+    int num3 = num1 + num2;
+    return num3;
+}
+
+void doSomething(int &num)
+{
+    cout << num << endl;
+    num += 5;
+    cout << num << endl;
+    num += 5;
+    cout << num << endl;
+}
+*/
 int main()
 {
     /*
@@ -28,36 +48,72 @@ int main()
     cin >> x;
     cout << x << endl;
     */
+    /*
+        int day;
+        cin >> day;
 
-    int day;
-    cin >> day;
+        switch (day)
+        {
+        case 1:
+            cout << "Monday";
+            break;
+        case 2:
+            cout << "Tuesday";
+            break;
+        case 3:
+            cout << "Wednesday";
+            break;
+        case 4:
+            cout << "Thursday";
+            break;
+        case 5:
+            cout << "Friday";
+            break;
+        case 6:
+            cout << "Saturday";
+            break;
+        case 7:
+            cout << "Sunday";
+            break;
+        default:
+            cout << "Invalid day!";
+        }
+        */
+    // int arr[5];
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    // cout << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << endl;
+    /*
+    int arr[3][5];
+    arr[1][3] = 78;
+    cout << arr[1][3] << endl;
+    cout << arr[0][0] << endl;
 
-    switch (day)
+    string s = "Sujal";
+    int n = s.size();
+    cout << n << endl;
+    cout << s[0] << endl;
+     */
+
+    // for (int i = 1; i <= 100; i++)
+    //{
+    //    cout << "shisu " << endl;
+    //}
+    /*
+    int i = 1;
+    while (i <= 100)
     {
-    case 1:
-        cout << "Monday";
-        break;
-    case 2:
-        cout << "Tuesday";
-        break;
-    case 3:
-        cout << "Wednesday";
-        break;
-    case 4:
-        cout << "Thursday";
-        break;
-    case 5:
-        cout << "Friday";
-        break;
-    case 6:
-        cout << "Saturday";
-        break;
-    case 7:
-        cout << "Sunday";
-        break;
-    default:
-        cout << "Invalid day!";
+        cout << "shisu loves sujal " << i << endl;
+        i++;
     }
-
+*/
+    /* printName();
+    int num1, num2;
+    cin >> num1 >> num2;
+    int res = sum(num1, num2);
+    cout << res << endl;
+    int num = 10;
+    doSomething(num);
+    cout << num << endl;
+    */
     return 0;
 }
