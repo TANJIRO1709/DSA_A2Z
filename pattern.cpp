a9a12980d8e0ea9b09436e4121884a48556b0f6e
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-05-06 23:43:00 +0530

    TANJIRO1709

--- pattern.cpp
+++ pattern.cpp
@@ -177,6 +177,29 @@ void print10(int n)
     }
 }
 */
+/*
+void print11(int n)
+{
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            start = 1;
+        }
+        else
+        {
+            start = 0;
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start;
+            start = 1 - start;
+        }
+        cout << endl;
+    }
+}
+*/
 int main()
 {
     // print1(10);
@@ -190,5 +213,6 @@ int main()
     //  print9(5);
     // print10(5);
     // print10(5);
+    // print11(5);
     return 0;
 }
