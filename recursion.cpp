b2ee54e67ccc2e79e45f7386ebc705e63411f409
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-05-26 22:44:54 +0530

    TANJIRO1709

--- recursion.cpp
+++ recursion.cpp
@@ -24,6 +24,7 @@ void f(int i, int sum)
     f(i - 1, sum + i);
 }
 */
+/*
 int fn(int n)
 {
     if (n == 0)
@@ -32,12 +33,31 @@ int fn(int n)
     }
     return n + fn(n - 1);
 }
+*/
+
+void f(int l, int a[], int n)
+{
+    if (l >= n / 2)
+    {
+        return;
+    }
+    swap(a[l], a[n - l - 1]);
+    f(l + 1, a, n);
+}
 int main()
 {
-    int n;
-    cin >> n;
-    cout << fn(n);
-    // f(n, 0);
-    //  name(n, 1);
+    // int n;
+    // cin >> n;
+    // cout << fn(n);
+    //  f(n, 0);
+    //   name(n, 1);
+    int n = 5;
+    int a[] = {1, 3, 6, 7, 9};
+    f(0, a, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " " << endl;
+    }
+
     return 0;
 }
