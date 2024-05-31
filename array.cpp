fd775ec8b6ea91d8089f17b76e31cf9f5a2dc6ed
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-05-31 20:57:28 +0530

    TANJIRO1709

--- array.cpp
+++ array.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 int main()
 {
+    // KADANE'S ALGORITHM
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int maxi = INT16_MIN;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+        if (sum < 0)
+            sum = 0;
+        if (sum > maxi)
+        {
+            maxi = sum;
+        }
+    }
+    cout << maxi;
     return 0;
 }
