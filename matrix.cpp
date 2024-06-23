b1517807f8e234c5cd024dca6c6a0ea280d87da8
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-06-23 12:52:01 +0530

    set matrix zero

--- matrix.cpp
+++ matrix.cpp
@@ -2,10 +2,51 @@
 using namespace std;
 int main()
 { // set matrix zeroes
-    int n;
-    int m;
+    int col0 = 1;
     for (int i = 0; i < n; i++)
     {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                matrix[i][0] = 0;
+            }
+            if (j != 0)
+            {
+                matrix[0][j] = 0;
+            }
+            else
+            {
+                col0 = 0;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    if (matrix[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[0][j] = 0;
+        }
+    }
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = 0;
+        }
     }
     return 0;
 }
